ec7fe1bee3343f2583b50e9556040913a64107c2
d.molchanov <dnb2011@yandex.ru>
2023-07-23 23:10:45 +0300

    fix: chekstyle неиспользуемый импорт

--- src/main/java/ru/practicum/shareit/exception/controller/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/controller/ErrorHandler.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.exception.controller;
 
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
