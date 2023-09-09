54fdb76e6b0d824a30aee94d2c60b49c499755c2
d.molchanov <dnb2011@yandex.ru>
2023-09-09 21:27:44 +0300

    checkstyle: Изменение методов на статические методы UserMapper

--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -10,11 +10,11 @@ import java.util.stream.Collectors;
 @UtilityClass
 public class UserMapper {
 
-    public UserDto toUserDto(User user) {
+    public static UserDto toUserDto(User user) {
         return new UserDto(user.getId(), user.getName(), user.getEmail());
     }
 
-    public User toUser(UserDto userDto, Integer id) {
+    public static User toUser(UserDto userDto, Integer id) {
         return User.builder()
                 .id(id)
                 .name(userDto.getName())
@@ -22,7 +22,7 @@ public class UserMapper {
                 .build();
     }
 
-    public Collection<UserDto> toUserDto(Collection<User> users) {
+    public static Collection<UserDto> toUserDto(Collection<User> users) {
         return users.stream()
                 .map(UserMapper::toUserDto)
                 .collect(Collectors.toList());
